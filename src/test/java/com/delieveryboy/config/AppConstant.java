bb392a302e7e98928944a52a219edf3eeebc977c
hayato <alokassembly1@gmail.com>
2025-08-27 20:14:25 +0000

    AppConstant has been created

---
+++ src/test/java/com/delieveryboy/config/AppConstant.java
@@ -0,0 +1,5 @@
+package com.delieveryboy.config;
+
+public class AppConstant {
+    public static final String LOCATION_TOPIC_NAME = "location_update_topic";
+}
--- src/test/java/com/delieveryboy/service/KafkaService.java
+++ src/test/java/com/delieveryboy/service/KafkaService.java
@@ -1,4 +1,12 @@
 package com.delieveryboy.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+
 public class KafkaService {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    
 }
