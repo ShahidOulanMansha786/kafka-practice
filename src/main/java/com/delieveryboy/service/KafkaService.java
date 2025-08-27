ca6f326ba992495fe416aa9278dbf1cfdfa83455
hayato <alokassembly1@gmail.com>
2025-08-27 20:41:41 +0000

    logger has added kafkaservice

--- src/main/java/com/delieveryboy/service/KafkaService.java
+++ src/main/java/com/delieveryboy/service/KafkaService.java
@@ -1,6 +1,8 @@
 package com.delieveryboy.service;
 
 import com.delieveryboy.config.AppConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 
@@ -9,8 +11,10 @@ public class KafkaService {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    private Logger log = LoggerFactory.getLogger(KafkaService.class);
+
     public void updateLocation(String location) {
         kafkaTemplate.send(AppConstant.LOCATION_TOPIC_NAME, location);
-
+        log.info("message produced");
     }
 }
